ba4830656814e5cba214b8df067f392266b87d2c
hugo-dev-vn <nguyenquoctuan61089@gmail.com>
2021-08-18 15:41:44 +0700

    commit 02-ticking: client - impl hepler buildClient

--- microservices/02-ticking/client/pages/index.js
+++ microservices/02-ticking/client/pages/index.js
@@ -1,35 +1,14 @@
-import axios from "axios";
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async({ req }) => {
-  console.log(req.headers);
-  if (typeof window === 'undefined') {
-    // we are on the server!
-    // request should be made to http://ingress-nginx-controller.ingress-nginx.....
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: {
-          // Host: 'ticketing.hugo2908-02-ticking-auth.com' // get from headers sent from browser
-          ...req.headers
-        }
-      }
-    );
-
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
-  }
-  
-  return {};
+LandingPage.getInitialProps = async(context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
 }
 
 export default LandingPage;
