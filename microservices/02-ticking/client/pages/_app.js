1b1a5e4a9aed166a4c4533e89a8e3c6accdf4f46
hugo-dev-vn <nguyenquoctuan61089@gmail.com>
2021-08-18 16:24:59 +0700

    commit 02-ticking: client, Header - resolve problem render of page and component - the way to pass props from root AppCompoent to page

--- microservices/02-ticking/client/pages/_app.js
+++ microservices/02-ticking/client/pages/_app.js
@@ -1,7 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-const app = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return <div>
+    <h1>Header1 {currentUser?.email}</h1>
+    <Component {...pageProps} />
+  </div>;
 };
 
-export default app;
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);  
+  }
+
+  console.log('AppComponent', pageProps);
+  
+  return {
+    pageProps,
+    ...data,
+  }
+};
+
+export default AppComponent;
