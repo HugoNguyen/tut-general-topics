111ded14c3ba6966a04db8032f966af440fd08db
hugo-dev-vn <nguyenquoctuan61089@gmail.com>
2021-08-21 20:24:31 +0700

    02-ticking: expiration - init - write depl

---
+++ microservices/02-ticking/expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,13 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, OrderCreatedEvent } from "@hugo-dev-vn/common";
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+
+    readonly subject = Subjects.OrderCreated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message): Promise<void> {
+        
+    }
+}
