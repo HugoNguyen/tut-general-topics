6f1957712aae655e9ee476bec078e8ab2e7b728b
hugo-dev-vn <nguyenquoctuan61089@gmail.com>
2021-08-21 11:21:12 +0700

    02-ticking: common
    - fix some typo

--- microservices/02-ticking/common/src/events/ticket-created-event.ts
+++ microservices/02-ticking/common/src/events/ticket-created-event.ts
@@ -1,6 +1,6 @@
 import { Subjects } from "./subjects";
 
-export interface TickedCreatedEvent {
+export interface TicketCreatedEvent {
     subject: Subjects.TicketCreated;
     data: {
         id: string;
