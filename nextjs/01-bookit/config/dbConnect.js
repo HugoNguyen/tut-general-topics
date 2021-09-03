9173ea5f50d31c0b01e9a3b7da2cead87cc8245c
hugo-dev-vn <nguyenquoctuan61089@gmail.com>
2021-09-03 15:51:50 +0000

    nextjs/01-bookit: - imp CURD room entity - imp helper seeder.js

--- nextjs/01-bookit/config/dbConnect.js
+++ nextjs/01-bookit/config/dbConnect.js
@@ -7,7 +7,7 @@ const dbConnect = () => {
 
     mongoose.connect(process.env.DB_LOCAL_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
+        useUnifiedTopology: true
     }).then(con => console.log('Connected to local db'));
 }
 
--- nextjs/01-bookit/controllers/roomControllers.js
+++ nextjs/01-bookit/controllers/roomControllers.js
@@ -1,10 +1,135 @@
-const allRooms = (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'All Rooms'
-    });
+import Room from '../models/rooms';
+
+const allRooms = async(req, res) => {
+    try {
+        const rooms = await Room.find(); 
+    
+        res.status(200).json({
+            success: true,
+            count: rooms.length,
+            rooms
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: false,
+            error: error.message,
+        })
+    }
+
+};
+
+// Create new room => /api/rooms 
+const newRoom = async(req, res) => {
+
+    try {
+        const room = await Room.create(req.body);
+
+        res.status(200).json({
+            success: true,
+            room,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: false,
+            error: error.message,
+        })
+    }
+    
+}
+
+// Get room details => /api/rooms/:id
+const getSingleRoom = async(req, res) => {
+
+    try {
+        const { id } = req.query;
+        const room = await Room.findById(id);
+
+        if (!room) {
+            res.status(400).json({
+                status: false,
+                error: 'Room not found with this id',
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            room,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: false,
+            error: error.message,
+        })
+    }
+    
+}
+
+// Update room details => /api/rooms/:id
+const updateRoom = async(req, res) => {
+
+    try {
+        const { id } = req.query;
+        let room = await Room.findById(id);
+
+        if (!room) {
+            res.status(400).json({
+                status: false,
+                error: 'Room not found with this id',
+            });
+        }
+
+        room = await Room.findByIdAndUpdate(id, req.body, {
+            new: true,
+            runValidators: true,
+            useFindAndModify: false,
+        })
+
+        res.status(200).json({
+            success: true,
+            room,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: false,
+            error: error.message,
+        })
+    }
+    
+}
+
+// Delete room => /api/rooms/:id
+const deleteRoom = async(req, res) => {
+
+    try {
+        const { id } = req.query;
+        const room = await Room.findById(id);
+
+        if (!room) {
+            res.status(400).json({
+                status: false,
+                error: 'Room not found with this id',
+            });
+        }
+
+        await room.remove();
+
+        res.status(200).json({
+            success: true,
+            message: 'Room is deleted'
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: false,
+            error: error.message,
+        })
+    }
+    
 }
 
 export {
     allRooms,
+    newRoom,
+    getSingleRoom,
+    updateRoom,
+    deleteRoom,
 }
