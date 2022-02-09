bdf86a0f634205e2427e3ec4136a5b4c7b436312
HugoDev <nguyenquoctuan61089@gmail.com>
2022-02-09 23:10:46 +0700

    blazor/bookstoreapp: complete setup blazor server ui - add authentication

---
+++ blazor/01-book-store-app/bookstoreapp.blazor.server.ui/Program.cs
@@ -0,0 +1,40 @@
+using Blazored.LocalStorage;
+using bookstoreapp.blazor.server.ui.Providers;
+using bookstoreapp.blazor.server.ui.Services.Authentication;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddHttpClient<bookstoreapp.blazor.server.ui.Services.Base.IClient, bookstoreapp.blazor.server.ui.Services.Base.Client>(cl => cl.BaseAddress = new Uri("https://localhost:7094"));
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+builder.Services.AddScoped<ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(p => 
+    p.GetRequiredService<ApiAuthenticationStateProvider>());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ blazor/01-book-store-app/bookstoreapp.blazor.server.ui/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using bookstoreapp.blazor.server.ui.Services.Base;
+
+namespace bookstoreapp.blazor.server.ui.Services.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> AuthenticateAsync(LoginUserDto loginModel);
+        Task Logout();
+    }
+}
