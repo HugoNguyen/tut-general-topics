4520d90cfe6d37170859a964ffe509f09d2c1273
HugoDev <nguyenquoctuan61089@gmail.com>
2023-03-20 23:10:33 +0700

    csharp-tdd/roombookingapp - step1, structure proj

---
+++ csharp-tdd/RoomBookingApp/RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Response_With_Request_Values()
+        {
+            //Arrange
+            var request = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                Date = new DateTime(2000, 1, 1)
+            };
+
+            var processor = new RoomBookingRequestProcessor();
+
+            //Act
+            RoomBookingResult result = processor.BookRoom(request);
+
+            //Assert
+            Assert.NotNull(result); //XUNIT
+            Assert.Equal(request.FullName, result.FullName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+
+            result.ShouldNotBeNull(); //Shouldly
+            result.FullName.ShouldBe(request.FullName);
+            result.Email.ShouldBe(request.Email);
+            result.Date.ShouldBe(request.Date);
+
+        }
+    }
+}
---
+++ csharp-tdd/RoomBookingApp/requirements.md
@@ -0,0 +1,15 @@
+1/ Return Result with Same Values as Request
+=> done
+
+2/ Processore Throw Null Exception if equest is empty
+=> inprocess
+
+3/ Save A Room Booking Record
+
+4/ Check Room Availability Before Attempting Booking
+
+5/ Store Booking Record with Room Identifier
+
+6/ Return Success Flag on Result
+
+7/ Add Room Booking Record Id to Result
