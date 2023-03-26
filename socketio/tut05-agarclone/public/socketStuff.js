7537046b073c6dba68e23a0e1b45fae345118916
Hugo Ng <nguyenquoctuan61089@gmail.com>
2023-03-26 18:54:51 +0700

    socketio/tut05-agarclone - refactor logic of init game

--- socketio/tut05-agarclone/public/socketStuff.js
+++ socketio/tut05-agarclone/public/socketStuff.js
@@ -1,6 +1,17 @@
 let socket = io.connect('/');
 
-socket.on('init', data => {
+// this fn is called when user clicks on the start button
+function init() {
+    // start drawing the screen
+    draw();
+
+    // call the init event when the client is ready for the data
+    socket.emit('init', {
+        playerName: player.name,
+    })
+}
+
+socket.on('initReturn', data => {
     // uiStuff.orbs
     orbs = data.orbs;
 });
