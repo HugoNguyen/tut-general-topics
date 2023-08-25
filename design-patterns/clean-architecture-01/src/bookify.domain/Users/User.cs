1382e49ce22287cf312cb4ebfce01954498c6ea1
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2023-08-25 23:23:26 +0700

    Clean Architecture 01: - add entities of User - instroduce Static Factory pattern to enhance encapsulation and prevent side effect when expose constructor out of my domain

---
+++ design-patterns/clean-architecture-01/src/bookify.domain/Users/User.cs
@@ -0,0 +1,27 @@
+﻿using bookify.domain.Abstractions;
+
+namespace bookify.domain.Users;
+public sealed class User : Entity
+{
+    private User(Guid id, FirstName firstName, LastName lastName, Email email)
+        : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    public FirstName FirstName { get; private set; }
+    public LastName LastName { get; private set; }
+    public Email Email { get; private set; }
+
+    /// <summary>
+    /// For encapsulation, hide constructor method
+    /// </summary>
+    /// <returns></returns>
+    public static User Create(FirstName firstName, LastName lastName, Email email)
+    {
+        var user = new User(Guid.NewGuid(), firstName, lastName, email);
+        return user;
+    }
+}
