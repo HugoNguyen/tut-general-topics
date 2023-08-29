eb526477cee2ede5b2d59b215956e0108a72c30c
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2023-08-29 21:50:11 +0700

    Clean Architecture 01: - create application - introduce DependencyInjection class

---
+++ design-patterns/clean-architecture-01/src/bookify.application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using bookify.domain.Bookings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace bookify.application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly);
+        });
+
+        services.AddTransient<PricingService>();
+
+        return services;
+    }
+}
