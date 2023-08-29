c63573d4b4b2a4e1d860e46016d4469d95f6e87b
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2023-08-29 22:01:09 +0700

    Clean Architecture 01: - add abstraction IQuery/ICommand

---
+++ design-patterns/clean-architecture-01/src/bookify.application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using bookify.domain.Abstractions;
+using MediatR;
+
+namespace bookify.application.Abstractions.Messaging;
+public interface ICommand : IRequest<Result>, IBaseCommand
+{
+}
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>, IBaseCommand
+{
+}
+
+/// <summary>
+/// To apply generic constraints
+/// </summary>
+public interface IBaseCommand
+{
+}
