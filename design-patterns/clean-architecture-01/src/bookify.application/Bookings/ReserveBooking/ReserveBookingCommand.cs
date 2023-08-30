b88953d4a7f0ebcba382fabcca4c32530098e6a8
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2023-08-30 20:56:35 +0700

    Clean Architecture 01: - Implement first Command, ReserveBookingCommand - introduce IDateTimeProvider

---
+++ design-patterns/clean-architecture-01/src/bookify.application/Bookings/ReserveBooking/ReserveBookingCommand.cs
@@ -0,0 +1,8 @@
+﻿using bookify.application.Abstractions.Messaging;
+
+namespace bookify.application.Bookings.ReserveBooking;
+public record ReserveBookingCommand(
+    Guid ApartmentId,
+    Guid UserId,
+    DateOnly StartDate,
+    DateOnly EndDate) : ICommand<Guid>;
---
+++ design-patterns/clean-architecture-01/src/bookify.domain/Bookings/IBookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using bookify.domain.Apartments;
+
+namespace bookify.domain.Bookings;
+public interface IBookingRepository
+{
+    Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<bool> IsOverlappingAsync(
+        Apartment apartment,
+        DateRange duration,
+        CancellationToken cancellationToken = default);
+
+    void Add(Booking booking);
+}
