697d755c93d14df9c2929440a6c11fe283f85f0a
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2023-08-31 20:35:33 +0700

    Clean Architecture 01: - add infrastructure layer - add DependencyInjection - add DateTimeProvider & EmailService

---
+++ design-patterns/clean-architecture-01/sr/bookify.infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using bookify.application.Abstractions.Clock;
+using bookify.application.Abstractions.Email;
+using bookify.infrastructure.Clock;
+using bookify.infrastructure.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace bookify.infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddTransient<IEmailService, EmailService>();
+
+        return services;
+    }
+}
