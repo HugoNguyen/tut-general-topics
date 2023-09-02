35c93f4b0b20e395647ea06eacadf705f670ce3b
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2023-09-02 13:03:05 +0700

    Clean Architecture 01: - apply unit test - architecture test, using NetArchTest.Rules

---
+++ design-patterns/clean-architecture-01/test/bookify.architecture.unitTests/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿using bookify.application.Abstractions.Messaging;
+using bookify.domain.Abstractions;
+using bookify.infrastructure;
+using System.Reflection;
+
+namespace bookify.architecture.unitTests;
+
+public class BaseTest
+{
+    protected static Assembly ApplicationAssembly => typeof(IBaseCommand).Assembly;
+
+    protected static Assembly DomainAssembly => typeof(IEntity).Assembly;
+
+    protected static Assembly InfrastructureAssembly => typeof(ApplicationDbContext).Assembly;
+}
---
+++ design-patterns/clean-architecture-01/test/bookify.architecture.unitTests/Domain/DomainTests.cs
@@ -0,0 +1,19 @@
+﻿using bookify.domain.Abstractions;
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace bookify.architecture.unitTests.Domain;
+public class DomainTests : BaseTest
+{
+    [Fact]
+    public void DomainEvent_Should_Have_DomainEventPostfix()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .That()
+            .ImplementInterface(typeof(IDomainEvent))
+            .Should().HaveNameEndingWith("DomainEvent")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
