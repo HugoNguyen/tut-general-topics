3d993332990fe5246f65eb23b20e1472493f85ec
Hugo Ng <nguyenquoctuan61089@gmail.com>
2023-12-09 13:14:47 +0700

    Observable Concatenation

--- rxjs-101/tut01-rxjs-course/src/app/about/about.component.ts
+++ rxjs-101/tut01-rxjs-course/src/app/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Observable, noop } from 'rxjs';
+import { Observable, concat, interval, noop, of } from 'rxjs';
 import { createHttpObservable } from '../common/util';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -16,6 +16,8 @@ export class AboutComponent implements OnInit {
 
     // this.sample01_BuildHttpObservable();
     // this.sample02_MapOperator();
+    // this.sample03_Concatenation01();
+    // this.sample03_Concatenation02();
   }
 
   sample01_BuildHttpObservable() {
@@ -55,5 +57,43 @@ export class AboutComponent implements OnInit {
       noop, // replace () => {},
       () => console.log('completed')
     )
+  }
+
+  /**
+   * Output
+   *  1 2 3 4 5 6 7 8 9
+   * Explanation:
+   *  source1$ will emit 1, 2, 3 and complete.
+   *  Then source2$ begin emit its values then complete
+   *  Then source3$ begin emit its values
+   */
+  sample03_Concatenation01() {
+    const source1$ = of(1, 2, 3);
+
+    const source2$ = of(4, 5, 6);
+
+    const source3$ = of(7, 8, 9);
+
+    const result$ = concat(source1$, source2$, source3$);
+
+    result$.subscribe(console.log);
+  }
+
+  /**
+   * Output
+   *  1 2 3 4 5 6 7 8 9
+   * Explanation:
+   *  source1$ will never completed. It means source2$, source3$ will never be emited
+   */
+  sample03_Concatenation02() {
+    const source1$ = interval(1000);
+
+    const source2$ = of('a', 'b', 'c');
+
+    const source3$ = of('d', 'e', 'f');
+
+    const result$ = concat(source1$, source2$, source3$);
+
+    result$.subscribe(console.log);
   }
 }
