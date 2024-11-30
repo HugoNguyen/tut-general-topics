8190900d7f2b3c8030286f60b582cd3e9fcca711
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2024-11-30 16:03:09 +0700

    angular/proj01: control flow - @for, @if

--- angular/proj01-essentials/src/app/app.component.ts
+++ angular/proj01-essentials/src/app/app.component.ts
@@ -13,10 +13,10 @@ import { TasksComponent } from "./tasks/tasks.component";
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
-    return this.users.find(el => el.id === this.selectedUserId)!;
+    return this.users.find(el => el.id === this.selectedUserId);
   }
 
   onSelectUser(id: string) {
