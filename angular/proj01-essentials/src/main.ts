7a90d72b76207e437c3caacd430750145715d896
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2024-11-30 09:27:50 +0700

    angular/proj01: add first custom component - create & use a standalone component

--- angular/proj01-essentials/src/main.ts
+++ angular/proj01-essentials/src/main.ts
@@ -2,4 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
+//Renders a standalone component (AppComponent) as the application's root component
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
