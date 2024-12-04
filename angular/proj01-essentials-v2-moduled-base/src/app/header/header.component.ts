9f3b071c9a2141855f6f2f81ad85003ed74d7ba7
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2024-12-04 20:39:13 +0700

    angular/proj01-moduled-base: - migrate others to use modules

--- angular/proj01-essentials-v2-moduled-base/src/app/header/header.component.ts
+++ angular/proj01-essentials-v2-moduled-base/src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
-    standalone: true, // true is default in >= angular v19
+    standalone: false,
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
--- angular/proj01-essentials-v2-moduled-base/src/app/tasks/task/task.component.ts
+++ angular/proj01-essentials-v2-moduled-base/src/app/tasks/task/task.component.ts
@@ -1,12 +1,10 @@
 import { Component, inject, Input } from '@angular/core';
-import { DatePipe } from '@angular/common';
 import { type Task } from './task.model';
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
-  imports: [DatePipe],
+  standalone: false,
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
