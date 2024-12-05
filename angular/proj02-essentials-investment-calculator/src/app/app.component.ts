2f8aafcadbeb3fd7e2b9e4aa2f27bc05f69ac312
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2024-12-05 23:08:07 +0700

    angular/proj02, investment calculator - add service

--- angular/proj02-essentials-investment-calculator/src/app/app.component.ts
+++ angular/proj02-essentials-investment-calculator/src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
 import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
-import { InvestmentInput } from './investment-input.model';
-import { InvestmentResult } from './investment-result.model';
 
 @Component({
   selector: 'app-root',
@@ -11,35 +9,4 @@ import { InvestmentResult } from './investment-result.model';
   templateUrl: './app.component.html',
   imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
-export class AppComponent {
-
-  resultsData = signal<InvestmentResult[] | undefined>(undefined);
-
-  onCalculateInvestmentResults({
-    initialInvestment,
-    annualInvestment,
-    expectedReturn,
-    duration,
-  }: InvestmentInput) {
-    const annualData: InvestmentResult[] = [];
-    let investmentValue = initialInvestment;
-  
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-  
-    this.resultsData.set(annualData);
-  }
-}
+export class AppComponent {}
