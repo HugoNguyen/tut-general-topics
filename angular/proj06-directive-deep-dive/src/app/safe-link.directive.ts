be6a606909496bfda65b1ae50908a64dffc70de2
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2024-12-21 19:11:57 +0700

    angular/proj06: directive deep dive - add custom [safelink] directive

---
+++ angular/proj06-directive-deep-dive/src/app/safe-link.directive.ts
@@ -0,0 +1,25 @@
+import { Directive } from "@angular/core";
+
+@Directive({
+    selector: 'a[appSafeLink]',
+    standalone: true,
+    host: {
+        '(click)': 'onConfirmLeavePage($event)'
+    }
+})
+export class SafeLinkDirective {
+    constructor() {
+        console.log('SafeLinkDirective is active');
+    }
+
+    onConfirmLeavePage(event: MouseEvent) {
+        const wantsToLeave = window.confirm('Are you sure you want to leave?');
+
+        if (wantsToLeave) {
+            return true;
+        }
+
+        event.preventDefault();
+        return false;
+    }
+}
