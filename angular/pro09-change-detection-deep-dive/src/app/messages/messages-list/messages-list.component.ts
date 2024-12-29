8665111d9dcafcf748c5d5d95f51575f3a0f18cd
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2024-12-29 11:24:01 +0700

    angular/proj09: Change detection deep dive - example of use async pipe to redunct code

--- angular/pro09-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
+++ angular/pro09-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
@@ -7,23 +8,11 @@ import { MessagesService } from '../messages.service';
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [AsyncPipe],
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef);
-  messages: string[] = [];
-
-  ngOnInit(): void {
-    const subscription = this.messagesService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    });
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
-
+  messages$ = this.messagesService.messages$;
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
