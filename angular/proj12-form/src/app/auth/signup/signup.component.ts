92ca944968e7e496930d57c2027f68a59e90c365
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-02-03 20:33:28 +0700

    angular/proj12: form - more about multi-input validator

--- angular/proj12-form/src/app/auth/signup/signup.component.ts
+++ angular/proj12-form/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { last } from 'rxjs';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+function equalValues (controlName1: string, controlName2: string) {
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value;
+    const val2 = control.get(controlName2)?.value;
+    if (val1 === val2) {
+      return null;
+    }
+    return { valuesNotEqual: true };
+  }
+}
+
 
 @Component({
   selector: 'app-signup',
@@ -21,6 +32,8 @@ export class SignupComponent {
       confirmPassword: new FormControl('', {
         validators: [ Validators.required, Validators.minLength(6) ],
       }),
+    }, {
+      validators: [ equalValues('password', 'confirmPassword') ],
     }),
     firstName: new FormControl('', {
       validators: [ Validators.required ],
@@ -56,6 +69,10 @@ export class SignupComponent {
   });
 
   onSubmit() {
+    if (this.form.invalid) {
+      console.log('Invalid form');
+      return;
+    }
     console.log(this.form.value);
   }
 
