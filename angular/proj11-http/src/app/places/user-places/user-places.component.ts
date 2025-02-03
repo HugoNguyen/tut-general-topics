ea5fbac265116dfa4627f85239aa89eba427285e
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-02-03 13:35:58 +0700

    angular/proj11: http - sending delete user-place request

--- angular/proj11-http/src/app/places/user-places/user-places.component.ts
+++ angular/proj11-http/src/app/places/user-places/user-places.component.ts
@@ -35,4 +35,12 @@ export class UserPlacesComponent {
       subscription.unsubscribe();
     });
   }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
