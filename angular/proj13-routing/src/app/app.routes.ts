8026ddbe050684e62d72656a7ed1ad991eb09f57
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-02-04 15:07:28 +0700

    angular/proj13: routing - add more routes - adding link right way with RouteLink

--- angular/proj13-routing/src/app/app.routes.ts
+++ angular/proj13-routing/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from "@angular/router";
 
 import { TasksComponent } from "./tasks/tasks.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 
 export const routes: Routes = [
+    {
+        path: '', // <domain>
+        component: NoTaskComponent,
+    },
     {
         path: 'tasks', // <domain>/tasks
         component: TasksComponent,
