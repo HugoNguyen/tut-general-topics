540f412cee1f67ab6eb3760f1cb4c83a0010fa86
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-02-04 17:18:48 +0700

    angular/proj13: routing - resolving reoute-related dynamic data

--- angular/proj13-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ angular/proj13-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterOutlet, RouterStateSnapshot } from '@angular/router';
 
 import { UsersService } from '../users.service';
 
@@ -10,7 +10,7 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent implements OnInit {
+export class UserTasksComponent {
   // Op1.1: Extracting Dynamic Route Parameters via @Input()
   // @Input({required: true}) userId!: string;
 
@@ -18,23 +18,36 @@ export class UserTasksComponent implements OnInit {
   // userId = input.required<string>();
   
   // Op2: Extracting Dynamic Route Parameters via ActiviatedRoute
-  userId = signal<string>('');
+  // userId = signal<string>('');
   message = input.required<string>();
+  // Extract data from route. Provided by resolve fn
+  userName = input.required<string>();
 
-  private usersService = inject(UsersService);
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
+  // private usersService = inject(UsersService);
+  // private activatedRoute = inject(ActivatedRoute);
+  // private destroyRef = inject(DestroyRef);
 
-  userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
+  // userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
 
-  ngOnInit() {
-    console.log('Input Data: ' + this.message());
-    const subscription = this.activatedRoute.paramMap.subscribe({
-      next: paramMap => {
-        this.userId.set(paramMap.get('userId') || '');
-      },
-    });
+  // ngOnInit() {
+  //   console.log('Input Data: ' + this.message());
+  //   const subscription = this.activatedRoute.paramMap.subscribe({
+  //     next: paramMap => {
+  //       this.userId.set(paramMap.get('userId') || '');
+  //     },
+  //   });
 
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  }
+  //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  // }
+}
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName = usersService
+    .users
+    .find(u => u.id === activatedRoute.paramMap.get('userId'))?.name || '';
+  return userName;
 }
