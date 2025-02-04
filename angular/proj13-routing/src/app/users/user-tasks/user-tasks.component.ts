b159c1af973a8b62091fccb5a323744fc9415424
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-02-04 15:40:53 +0700

    angular/proj13: routing - Extracting Dynamic Route Parameters via ActiviatedRoute - support old angular version

--- angular/proj13-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ angular/proj13-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { UsersService } from '../users.service';
 
@@ -8,13 +9,29 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
-  // Extracting Dynamic Route Parameters via @Input()
+export class UserTasksComponent implements OnInit {
+  // Op1.1: Extracting Dynamic Route Parameters via @Input()
   // @Input({required: true}) userId!: string;
+
+  // Op1.2: Extracting Dynamic Route Parameters via input()
+  // userId = input.required<string>();
   
-  userId = input.required<string>();
-  
+  // Op2: Extracting Dynamic Route Parameters via ActiviatedRoute
+  userId = signal<string>('');
+
   private usersService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
 
   userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
+
+  ngOnInit() {
+    const subscription = this.activatedRoute.paramMap.subscribe({
+      next: paramMap => {
+        this.userId.set(paramMap.get('userId') || '');
+      },
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
