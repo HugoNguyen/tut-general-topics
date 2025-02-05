3b0bda760fb86ef5ff1a3d41aa08c4dd2c288a76
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-02-05 21:04:13 +0700

    angular/proj18: Programming rendering - startup project

---
+++ angular/proj18-programmatically-rendering-components/src/app/example01/using-ng-component-outlet/using-ng-component-outlet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-using-ng-component-outlet',
+  standalone: true,
+  imports: [],
+  templateUrl: './using-ng-component-outlet.component.html',
+  styleUrl: './using-ng-component-outlet.component.css'
+})
+export class UsingNgComponentOutletComponent {
+
+}
---
+++ angular/proj18-programmatically-rendering-components/src/app/example02/using-view-container-ref/using-view-container-ref.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-using-view-container-ref',
+  standalone: true,
+  imports: [],
+  templateUrl: './using-view-container-ref.component.html',
+  styleUrl: './using-view-container-ref.component.css'
+})
+export class UsingViewContainerRefComponent {
+
+}
