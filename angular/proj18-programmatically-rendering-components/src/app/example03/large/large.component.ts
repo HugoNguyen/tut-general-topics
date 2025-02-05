374810f892708ac665976170500be1c15b09cd89
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-02-05 22:10:16 +0700

    angular/proj18: Programming rendering - perform lazy load with NgComponentOutlet or ViewContainerRef

---
+++ angular/proj18-programmatically-rendering-components/src/app/example03/large/large.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-large',
+  standalone: true,
+  imports: [],
+  templateUrl: './large.component.html',
+  styleUrl: './large.component.css'
+})
+export class LargeComponent {
+
+}
--- angular/proj18-programmatically-rendering-components/src/app/example03/lazy-loading/lazy-loading.component.ts
+++ angular/proj18-programmatically-rendering-components/src/app/example03/lazy-loading/lazy-loading.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgComponentOutlet } from '@angular/common';
+import { Component, Type } from '@angular/core';
 
 @Component({
   selector: 'app-lazy-loading',
   standalone: true,
-  imports: [],
+  imports: [NgComponentOutlet, AsyncPipe],
   templateUrl: './lazy-loading.component.html',
   styleUrl: './lazy-loading.component.css'
 })
 export class LazyLoadingComponent {
 
+  componentRef!: Type<any>;
+  time = 0;
+
+  constructor() {
+    this.lazyRender();
+  }
+
+  async lazyRender() {
+    const start = new Date();
+    const { LargeComponent } = await import('../large/large.component');
+
+    this.componentRef = LargeComponent;
+    this.time = new Date().getTime() - start.getTime();
+  }
 }
