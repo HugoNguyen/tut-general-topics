2ceb31f829e5fa0298b31494b61e6b0b5c15ec75
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-02-08 21:16:40 +0700

    angular/proj20: Ngrx course - enable runtimecheck

--- angular/proj20-ngrx-course/src/app/app.module.ts
+++ angular/proj20-ngrx-course/src/app/app.module.ts
@@ -55,7 +55,18 @@ const routes: Routes = [
       MatListModule,
       MatToolbarModule,
       AuthModule.forRoot(),
-      StoreModule.forRoot(reducers, { metaReducers}),
+      StoreModule.forRoot(
+        reducers,
+        {
+          metaReducers,
+          runtimeChecks: {
+            strictStateImmutability: true,
+            strictActionImmutability: true,
+            strictActionSerializability: true,
+            strictStateSerializability: true
+          }
+        }
+      ),
       StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
       EffectsModule.forRoot([]),
       StoreRouterConnectingModule.forRoot({
