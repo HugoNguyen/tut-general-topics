109ec41ffbaafd836b9e60f0751d0ead9cfe1cb6
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-03-04 21:24:58 +0700

    redis/course-01/proj01: unexpected behaviour of HSET & HGETALL

--- redis/course-01-stephen-grider/proj01-rbay/sandbox/index.ts
+++ redis/course-01-stephen-grider/proj01-rbay/sandbox/index.ts
@@ -1,5 +1,26 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    const data: any = {
+        color: 'red',
+        year: 1950,
+        engine: { cylinders: 8 },
+        owner: '',
+        service: '',
+    };
+    await client.hSet('car', data);
+
+    const car = await client.hGetAll('car');
+
+    console.log(car);
+
+    // Get not existing key
+    const notExistingKey = await client.hGetAll('car#100000');
+    if (Object.keys(notExistingKey).length === 0) {
+        console.log('Car not found, respond with 404');
+    } else {
+        console.log('Car found');
+    }
+};
 run();
