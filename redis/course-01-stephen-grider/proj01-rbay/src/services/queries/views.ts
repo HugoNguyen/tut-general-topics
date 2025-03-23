ecf21c438c5f4ac9e6cae5d21b44334e10543a10
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-03-23 12:19:58 +0700

    redis/course-01/proj01: turn fn incrementView to LUA script

--- redis/course-01-stephen-grider/proj01-rbay/src/services/queries/views.ts
+++ redis/course-01-stephen-grider/proj01-rbay/src/services/queries/views.ts
@@ -1,11 +1,12 @@
 import { client } from '$services/redis';
-import {
-    itemsKey,
-    itemsByViewsKey,
-    itemsViewsKey,
-} from '$services/keys';
+// import {
+//     itemsKey,
+//     itemsByViewsKey,
+//     itemsViewsKey,
+// } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
+    /*
     const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
 
     if (inserted) {
@@ -14,4 +15,17 @@ export const incrementView = async (itemId: string, userId: string) => {
             client.zIncrBy(itemsByViewsKey(), 1, itemId)
         ]);
     }
+    */
+    return client.incrementView(itemId, userId);
 };
+
+
+// Keys I need to access
+// 1) itemsViewsKey
+// 2) itemsKey
+// 3) itemsBYViewsKey
+// EVALSHA <ID> 3 'itemsViewsKey' 'itemsKey' 'itemsByViewsKey'
+
+// Arguments I need to accept
+// 1) itemId
+// 2) userId
