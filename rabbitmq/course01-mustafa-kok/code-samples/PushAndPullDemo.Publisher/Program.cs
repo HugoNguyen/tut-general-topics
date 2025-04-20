4ffaadc4809133d8ade5ec21be22d4fb23b8d1fe
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-04-20 08:36:24 +0700

    rabbitmq/course01: add code sample of push and pull model

---
+++ rabbitmq/course01-mustafa-kok/code-samples/PushAndPullDemo.Publisher/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace PushAndPullDemo.Publisher
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            //1. Set up connection and channel
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "admin";
+            factory.Password = "passw@rd";
+
+            IConnection conn = await factory.CreateConnectionAsync();
+            IChannel channel = await conn.CreateChannelAsync();
+
+            //2. Create exchange and queue
+            await channel.ExchangeDeclareAsync("ex.fanout", ExchangeType.Fanout, durable: true, autoDelete: false);
+            await channel.QueueDeclareAsync("my.queue1", durable: true, exclusive: false, autoDelete: false);
+
+            //3. Binding exchange and queue
+            await channel.QueueBindAsync("my.queue1", "ex.fanout", "");
+
+            //4. Publish messing
+            while (true)
+            {
+                Console.WriteLine("Enter enter to exit");
+                Console.Write("Enter message:");
+                var message = Console.ReadLine();
+                if (message == null || message.Trim() == string.Empty)
+                {
+                    continue;
+                }
+                if (message == "exit")
+                {
+                    break;
+                }
+                await channel.BasicPublishAsync("ex.fanout", "", Encoding.UTF8.GetBytes(message));
+            }
+
+            //5. Delete queue and exchange
+            await channel.QueueDeleteAsync("my.queue1");
+            await channel.ExchangeDeleteAsync("ex.fanout");
+
+            //6. Close channel and connection
+            await channel.CloseAsync();
+            await conn.CloseAsync();
+            await channel.DisposeAsync();
+            await conn.DisposeAsync();
+        }
+    }
+}
---
+++ rabbitmq/course01-mustafa-kok/code-samples/PushAndPullDemo.PullConsumer/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace PushAndPullDemo.PullConsumer
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            //1. Set up connection and channel
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "admin";
+            factory.Password = "passw@rd";
+
+            IConnection conn = await factory.CreateConnectionAsync();
+            IChannel channel = await conn.CreateChannelAsync();
+
+            //2. Create consumer
+
+            //3. Bind consumer to queue
+
+            //4. Wait message
+            Console.WriteLine("Reading messages from queue. Press any key to exit.");
+
+            while (true)
+            {
+                Console.WriteLine("Trying to get a message from the queue...");
+
+                BasicGetResult? result = await channel.BasicGetAsync("my.queue1", true);
+
+                if (result != null)
+                {
+                    var body = result.Body.ToArray();
+                    string message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"Message: {message}");
+
+                    if (Console.KeyAvailable)
+                    {
+                        var keyInfo = Console.ReadKey();
+                        if (keyInfo.KeyChar == 'e' || keyInfo.KeyChar == 'E')
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                await Task.Delay(2000);
+            }
+
+            //5. Close channel and connection
+            await channel.CloseAsync();
+            await conn.CloseAsync();
+        }
+    }
+}
