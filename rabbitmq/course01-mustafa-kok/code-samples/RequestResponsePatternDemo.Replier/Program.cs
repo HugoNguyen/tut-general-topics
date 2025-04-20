dc39bbd92ad7c9ea8e8b0f98811911fe558dd80d
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-04-20 20:08:10 +0700

    rabbitmq/course01: add code sample of publish-subscribe pattern - simple demo, one request - one response

---
+++ rabbitmq/course01-mustafa-kok/code-samples/RequestResponsePatternDemo.Replier/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RequestResponsePatternDemo.Replier
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            //1. Set up connection and channel
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "admin";
+            factory.Password = "passw@rd";
+
+            IConnection conn = await factory.CreateConnectionAsync();
+            IChannel channel = await conn.CreateChannelAsync();
+
+            //2. Create consumer
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (ch, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                string request = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Request received: {request}");
+
+                var response = $"Response for {request}";
+
+                await channel.BasicPublishAsync("", "my.responses", Encoding.UTF8.GetBytes(response));
+            };
+
+            //3. Bind consumer to queue
+            var consumerTag = await channel.BasicConsumeAsync("my.requests", true, consumer);
+
+            //4. Wait message
+            Console.WriteLine($"Press any key to exit.");
+            Console.ReadKey();
+
+            //5. Close channel and connection
+            await channel.CloseAsync();
+            await conn.CloseAsync();
+        }
+    }
+}
---
+++ rabbitmq/course01-mustafa-kok/code-samples/RequestResponsePatternDemo.Requestor/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RequestResponsePatternDemo.Requestor
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            //1. Set up connection and channel
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "admin";
+            factory.Password = "passw@rd";
+
+            IConnection conn = await factory.CreateConnectionAsync();
+            IChannel channel = await conn.CreateChannelAsync();
+
+            //2. Create consumer
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (ch, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Response received: {message}");
+                await Task.CompletedTask;
+            };
+
+            //3. Bind consumer to queue
+            var consumerTag = await channel.BasicConsumeAsync("my.responses", true, consumer);
+
+            //4. Send messages to response
+            while (true)
+            {
+                Console.Write("Enter your request: ");
+                var request = Console.ReadLine();
+
+                if (request == null || request == string.Empty)
+                {
+                    continue;
+                }
+
+                if (request == "exit")
+                {
+                    break;
+                }
+
+                await channel.BasicPublishAsync("", "my.requests", Encoding.UTF8.GetBytes(request));
+            }
+
+            //5. Close channel and connection
+            await channel.CloseAsync();
+            await conn.CloseAsync();
+        }
+    }
+}
