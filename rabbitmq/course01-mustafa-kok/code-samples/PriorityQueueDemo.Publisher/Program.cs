9ddfc0a1c85eb4fc2b757afce51ec1904d526a9e
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-04-21 18:51:34 +0700

    rabbitmq/course01: add code sample of priority queue - demo of case 1, prefect not set, ack set to auto

---
+++ rabbitmq/course01-mustafa-kok/code-samples/PriorityQueueDemo.Publisher/Program.cs
@@ -0,0 +1,79 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace PriorityQueueDemo.Publisher
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            //1. Set up connection and channel
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "admin";
+            factory.Password = "passw@rd";
+
+            IConnection conn = await factory.CreateConnectionAsync();
+            IChannel channel = await conn.CreateChannelAsync();
+
+            //2. Create exchange and queue
+            await channel.ExchangeDeclareAsync("ex.fanout", ExchangeType.Fanout, durable: true, autoDelete: false);
+
+            //Create a queu that supports message priorities (with max message priority=5)
+            var queueArguments = new Dictionary<string, object?>
+            {
+                { "x-max-priority", 2 }
+            };
+            
+            await channel.QueueDeclareAsync("my.queue", durable: true, exclusive: false, autoDelete: false, arguments: queueArguments);
+
+            //3. Binding exchange and queue
+            await channel.QueueBindAsync("my.queue", "ex.fanout", "");
+
+            //4. Publish messing
+            Console.WriteLine("Publisher is ready. Press a key to start sending messages");
+            Console.ReadKey();
+
+            //Send sample message with low (priority=1) and high (priority=5) priorities
+
+            //Low priority messages
+            await sendMessage(channel, 1);
+            await sendMessage(channel, 1);
+            await sendMessage(channel, 1);
+
+            //High priority messages
+            await sendMessage(channel, 2);
+            await sendMessage(channel, 2);
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+
+            //5. Delete queue and exchange
+            await channel.QueueDeleteAsync("my.queue");
+            await channel.ExchangeDeleteAsync("ex.fanout");
+
+            //6. Close channel and connection
+            await channel.CloseAsync();
+            await conn.CloseAsync();
+            await channel.DisposeAsync();
+            await conn.DisposeAsync();
+        }
+
+        private static async Task sendMessage(IChannel channel, byte priority)
+        {
+            var message = $"Message with priority={priority}";
+            await channel.BasicPublishAsync(
+                exchange: "ex.fanout",
+                routingKey: "",
+                mandatory: true,
+                basicProperties: new BasicProperties
+                {
+                    Priority = priority,
+                },
+                body: Encoding.UTF8.GetBytes(message));
+            Console.WriteLine($"SENT: {message}");
+        }
+    }
+}
---
+++ rabbitmq/course01-mustafa-kok/code-samples/PriorityQueueDemo.Worker/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace PriorityQueueDemo.Worker
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            //1. Set up connection and channel
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "admin";
+            factory.Password = "passw@rd";
+
+            IConnection conn = await factory.CreateConnectionAsync();
+            IChannel channel = await conn.CreateChannelAsync();
+
+            //2. Create consumer
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (ch, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+                Console.Write($"Processing message -> '{message}'...");
+                await Task.Delay(1000);
+                Console.WriteLine("FINISHED");
+                //await channel.BasicAckAsync(ea.DeliveryTag, false);
+            };
+
+            //3. Bind consumer to queue
+            var consumerTag = await channel.BasicConsumeAsync(
+                queue: "my.queue",
+                autoAck: true,
+                consumer: consumer);
+
+            //4. Wait message
+            Console.WriteLine("Subcribe to the queue. Press any key to exit.");
+            Console.ReadKey();
+
+            //5. Close channel and connection
+            await channel.CloseAsync();
+            await conn.CloseAsync();
+        }
+    }
+}
