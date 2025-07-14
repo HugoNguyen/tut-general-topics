2a7f823ff7a45d4ab8ca447257e7a8fd43d9aaac
IDEALOGIC\tuan-quoc.nguyen <tuan-quoc.nguyen@idealogic.com.vn>
2025-07-14 16:53:11 +0700

    design-patterns/course04: extract tick module as service

---
+++ design-patterns/course04-modular-monolith-architecture/evently/src/API/Evently.Ticketing.Api/OpenTelemetry/DiagnosticsConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Evently.Ticketing.Api.OpenTelemetry;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "<Pending>")]
+public static class DiagnosticsConfig
+{
+    public const string ServiceName = "Evently.Ticketing.Api";
+}
--- design-patterns/course04-modular-monolith-architecture/evently/test/Evently.ArchitectureTests/Layers/ModuleTests.cs
+++ design-patterns/course04-modular-monolith-architecture/evently/test/Evently.ArchitectureTests/Layers/ModuleTests.cs
@@ -2,10 +2,8 @@
 using Evently.ArchitectureTests.Abstractions;
 using Evently.Modules.Attendance.Domain.Attendees;
 using Evently.Modules.Attendance.Infrastructure;
-using Evently.Modules.Events.Domain.Events;
+using Evently.Modules.Events.Domain.TicketTypes;
 using Evently.Modules.Events.Infrastructure;
-using Evently.Modules.Ticketing.Domain.Orders;
-using Evently.Modules.Ticketing.Infrastructure;
 using Evently.Modules.Users.Domain.Users;
 using Evently.Modules.Users.Infrastructure;
 using NetArchTest.Rules;
@@ -18,12 +16,10 @@ public class ModuleTests : BaseTest
     public void UsersModule_ShouldNotHaveDependencyOn_AnyOtherModule()
     {
         string[] otherModules = [EventsNamespace, TicketingNamespace, AttendanceNamespace];
-        string[] integrationEventsModules =
-        [
+        string[] integrationEventsModules = [
             EventsIntegrationEventsNamespace,
             TicketingIntegrationEventsNamespace,
-            AttendanceIntegrationEventsNamespace
-        ];
+            AttendanceIntegrationEventsNamespace];
 
         List<Assembly> usersAssemblies =
         [
@@ -46,16 +42,14 @@ public class ModuleTests : BaseTest
     public void EventsModule_ShouldNotHaveDependencyOn_AnyOtherModule()
     {
         string[] otherModules = [UsersNamespace, TicketingNamespace, AttendanceNamespace];
-        string[] integrationEventsModules =
-        [
+        string[] integrationEventsModules = [
             UsersIntegrationEventsNamespace,
             TicketingIntegrationEventsNamespace,
-            AttendanceIntegrationEventsNamespace
-        ];
+            AttendanceIntegrationEventsNamespace];
 
         List<Assembly> eventsAssemblies =
         [
-            typeof(Event).Assembly,
+            typeof(TicketType).Assembly,
             Modules.Events.Application.AssemblyReference.Assembly,
             Modules.Events.Presentation.AssemblyReference.Assembly,
             typeof(EventsModule).Assembly
@@ -71,43 +65,13 @@ public class ModuleTests : BaseTest
     }
 
     [Fact]
-    public void TicketingModule_ShouldNotHaveDependencyOn_AnyOtherModule()
-    {
-        string[] otherModules = [EventsNamespace, UsersNamespace, AttendanceNamespace];
-        string[] integrationEventsModules =
-        [
-            EventsIntegrationEventsNamespace,
-            UsersIntegrationEventsNamespace,
-            AttendanceIntegrationEventsNamespace
-        ];
-
-        List<Assembly> ticketingAssemblies =
-        [
-            typeof(Order).Assembly,
-            Modules.Ticketing.Application.AssemblyReference.Assembly,
-            Modules.Ticketing.Presentation.AssemblyReference.Assembly,
-            typeof(TicketingModule).Assembly
-        ];
-
-        Types.InAssemblies(ticketingAssemblies)
-            .That()
-            .DoNotHaveDependencyOnAny(integrationEventsModules)
-            .Should()
-            .NotHaveDependencyOnAny(otherModules)
-            .GetResult()
-            .ShouldBeSuccessful();
-    }
-
-    [Fact]
     public void AttendanceModule_ShouldNotHaveDependencyOn_AnyOtherModule()
     {
-        string[] otherModules = [UsersNamespace, TicketingNamespace, EventsNamespace];
-        string[] integrationEventsModules =
-        [
+        string[] otherModules = [UsersNamespace, EventsNamespace, TicketingNamespace];
+        string[] integrationEventsModules = [
             UsersIntegrationEventsNamespace,
-            TicketingIntegrationEventsNamespace,
-            EventsIntegrationEventsNamespace
-        ];
+            EventsIntegrationEventsNamespace,
+            TicketingIntegrationEventsNamespace];
 
         List<Assembly> attendanceAssemblies =
         [
